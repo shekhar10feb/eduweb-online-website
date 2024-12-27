b1a59b3d6c0eb3656829738b3993b1afddbc9ffc
Shekhar Ranjan <shekhar10feb@gmail.com>
2024-12-27 15:07:12 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        // target: "https://eduweb-online-website.onrender.com",
-        target: "http://localhost:3001",
+        target: "https://eduweb-online-website.onrender.com",
+        // target: "http://localhost:3001",
         changeOrigin: true,
         secure: false,
         // rewrite: (path) => path.replace(/^\/api/, ""),
